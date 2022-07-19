97b9989c8a37a6567d5b7aaadc74f68e4e354862
송상훈 <sangong3@naver.com>
2022-07-19 14:35:16 +0900

    FEAT: springBoot setting

---
+++ A301/A301/src/test/java/com/chron/A301/A301ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chron.A301;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A301ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
