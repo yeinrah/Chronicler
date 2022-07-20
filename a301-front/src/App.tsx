b81a48af125db5dea14caa2fab0a810e2be126a6
kimjeongseo <wjdtj9656@gmail.com>
2022-07-20 17:47:59 +0900

    main text + signup/signin btn (need update)

--- a301-front/src/App.tsx
+++ a301-front/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import MainNonLog from './Pages/MainNonLog/MainNonLog';
 
 function App() {
   return (
     <div className="App">
+      <MainNonLog/>
     </div>
   );
 }
--- a301-front/src/Pages/MainNonLog/MainNonLog.tsx
+++ a301-front/src/Pages/MainNonLog/MainNonLog.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
-import 'MainNonLog.module.css';
-
-const MainNonLog = () => {
+import './MainNonLog.module.css';
+import {
+  Stack,
+  Card,
+  CardContent,
+  Typography,
+  Button,
+  styled,
+} from '@mui/material';
+const MainNonLog: React.FC = () => {
+  const MainStack = styled(Stack)({
+    marginTop: '40vh',
+    justifyContent: 'center',
+    alignItems: 'center',
+    spacing: '25vw',
+  });
+  const MainBtn = styled(Button)({
+    margin: '1vh',
+    color: '#FFFFFF',
+    backgroundColor: '#202321',
+  });
   return (
-    <div>MainNonLog</div>
-  )
-}
+    <>
+      <MainStack direction={'row'}>
+        <Card>
+          <CardContent>
+            <Typography sx={{ fontSize: '4vw' }}>회의에 집중하세요</Typography>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent sx={{ backgroundColor: '#715C71' }}>
+            <MainBtn variant="contained">로그인</MainBtn>
+            <br />
+            <MainBtn variant="contained">회원가입</MainBtn>
+          </CardContent>
+        </Card>
+      </MainStack>
+    </>
+  );
+};
 
-export default MainNonLog
+export default MainNonLog;
