8b1187d08b2ad7d139aa87e4ea8636b4079f282e
wYuYw <Luchsoa@gmail.com>
2022-07-21 10:14:18 +0900

    STYLE: change main background

--- a301-front/src/Containers/SignInUpCard/SignInUpCard.tsx
+++ a301-front/src/Containers/SignInUpCard/SignInUpCard.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './SignInUpCard.module.css';
+import {
+  Typography,
+  AppBar,
+  Toolbar,
+  styled,
+  Box,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+import { Menu as MenuIcon } from '@mui/icons-material';
 
 const SignInUpCard = () => {
-  return (
-    <div>SignInUpCard</div>
-  )
-}
+  return <div>SignInUpCard</div>;
+};
 
-export default SignInUpCard
+export default SignInUpCard;
