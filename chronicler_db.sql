6f22dcfb519482dc7cb9a9833351ec575953f63f
Yein Rah <yein.rah@gmail.com>
2022-07-25 15:42:51 +0900

    DOCS: update SQL Query

--- chronicler_db.sql
+++ chronicler_db.sql
@@ -1,12 +1,15 @@
-drop database IF EXISTS `chronicler`;
-create database IF NOT EXISTS `chronicler` collate utf8mb4_general_ci;
+DROP DATABASE IF EXISTS `chronicler`;
+
+CREATE DATABASE IF NOT EXISTS `chronicler` collate utf8mb4_general_ci;
+
 USE `chronicler`;
+
 CREATE TABLE user (
  id INTEGER auto_increment,
  nickname VARCHAR(32),
  password VARCHAR(255),
  email VARCHAR(255),  
- image VARCHAR(255),
+ image INTEGER DEFAULT 0,
  phone VARCHAR(11),
  PRIMARY KEY(id)
 );
