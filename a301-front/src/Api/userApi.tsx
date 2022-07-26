18fe5efa8bbcb924e0f5ff96971dcca62d37c2a9
kimjeongseo <wjdtj9656@gmail.com>
2022-07-26 13:54:45 +0900

    STYLE: FE API FORMAT EX

---
+++ a301-front/src/Api/userApi.tsx
@@ -0,0 +1,19 @@
+// import axios from 'axios';
+// const UserApi = axios.create({
+//   // baseURL: `www.naver.com`,
+//   baseURL: `http://localhost:8080`,
+//   timeout: 30000,
+//   headers: {
+//     'Content-Type': 'application/json;charset=UTF-8',
+//   },
+// });
+// export default UserApi;
+
+// 사용 예시
+// const Problem = (props) => {
+//   const deleteProblem = () => {
+//     ProblemApi.delete(`/problem/${props.item.id}`).then(() => {
+//       Swal.fire("삭제 성공");
+//       props.showItems();
+//     });
+//   };
