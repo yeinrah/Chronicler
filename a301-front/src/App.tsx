dd0a4f37a4daea4180f787c4f525c5baa5a115b5
kimjeongseo <wjdtj9656@gmail.com>
2022-08-01 17:53:35 +0900

    FEAT: change class->function

--- a301-front/src/App.tsx
+++ a301-front/src/App.tsx
@@ -24,6 +24,7 @@ import {
 import MRTest from './Pages/MRTest/MRTest';
 import { useRecoilState } from 'recoil';
 import showNavState from './recoil/atoms/showNavState';
+import MRTest2 from './Pages/MRTest/MRTest2';
 function App() {
   const [isShownNavState, setIsShownNavState] =
     useRecoilState<any>(showNavState);
@@ -39,7 +40,8 @@ function App() {
         <Route path="/mypage" element={<MyPage />} />
         <Route path="*" element={<ErrorPage />} />
         <Route path="/MeetingRoom" element={<MeetingRoom />} />
-        <Route path="/MRTest" element={<MRTest />} />
+        {/* <Route path="/MRTest" element={<MRTest />} /> */}
+        <Route path="/MRTest" element={<MRTest2 />} />
       </Routes>
     </div>
   );
