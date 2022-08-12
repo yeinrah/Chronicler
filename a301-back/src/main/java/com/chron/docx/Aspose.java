5ea903dd1c746b07f328cef8c911d43e04c58442
송상훈 <sangong3@naver.com>
2022-08-12 18:27:49 +0900

    FEAT: chart send email

--- a301-back/src/main/java/com/chron/docx/Aspose.java
+++ a301-back/src/main/java/com/chron/docx/Aspose.java
@@ -1,18 +1,67 @@
 package com.chron.docx;
 
-import org.springframework.stereotype.Component;
-
-import com.aspose.words.*;
-import com.chron.db.entity.Message;
-import com.chron.komoran.KomoranSearch;
-
 import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Scanner;
+
+import org.springframework.stereotype.Component;
+
+import com.aspose.words.BreakType;
+import com.aspose.words.Chart;
+import com.aspose.words.ChartDataLabelCollection;
+import com.aspose.words.ChartSeries;
+import com.aspose.words.ChartSeriesCollection;
+import com.aspose.words.ChartType;
+import com.aspose.words.Document;
+import com.aspose.words.DocumentBuilder;
+import com.aspose.words.HeightRule;
+import com.aspose.words.ParagraphAlignment;
+import com.aspose.words.Shape;
+import com.aspose.words.Style;
+import com.aspose.words.StyleType;
+import com.aspose.words.Table;
+import com.chron.db.entity.Message;
+import com.chron.komoran.KomoranSearch;
+import com.chron.wordcloud.words.StringProcessor;
+import com.chron.wordcloud.words.WordCount;
 
 @Component
 public class Aspose {
+	private static final String FILTER = "korean_filtering.txt";
+
+	private static final String[] POSITIVE = { "최고", "칭찬", "좋", "굿", "굳", "오케이", "짱", "퍼펙트", "나이스", "사랑", "희망", "성공" };
+	private static final String[] NEGATIVE = { "못", "안", "않", "싫", "혐", "바보" };
+
+	public String listToStrTotal(List<Message> chronicleData) {
+		KomoranSearch KS = new KomoranSearch();
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < chronicleData.size(); i++) {
+			List<String> ls = KS.makeKomoran(chronicleData.get(i).getText());
+			for (int j = 0; j < ls.size(); j++) {
+				sb.append(ls.get(j)).append(" ");
+			}
+		}
+		System.out.println("투스트링 : " + sb.toString());
+		return sb.toString();
+	}
+
+	public String listToStrTotalNotKomo(List<Message> chronicleData) {
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < chronicleData.size(); i++) {
+			String str = chronicleData.get(i).getText();
+			sb.append(str).append(" ");
+		}
+		System.out.println("투스트링NotKomo : " + sb.toString());
+		return sb.toString();
+	}
 
 	public void makeChronicle(List<Message> chronicleData, String date, String participants) throws Exception {
 		String timeData = date;
@@ -49,42 +98,43 @@ public class Aspose {
 		builder.write("시간 : " + time + "\r\r\r");
 		builder.write("참석자 :" + participants);
 		builder.insertBreak(BreakType.PAGE_BREAK);
+
+		// 1번차트(단어 빈도)
 		Shape shape = builder.insertChart(ChartType.COLUMN, 432, 252);
+
+		// 2번 차트(참여자 발화 빈도)
+		Shape shape2 = builder.insertChart(ChartType.PIE, 200, 220);
+
+		// 3번 차트(참여자 별 긍정어휘 차트)
+		Shape shape3 = builder.insertChart(ChartType.PIE, 200, 220);
 
 //		builder.insertImage(wordpath);
 		builder.insertBreak(BreakType.PAGE_BREAK);
 
-		// Column chart
-
-		// For complete examples and data files, please go to
-		// https://github.com/aspose-words/Aspose.Words-for-Java
-
-		// Add chart with default data. You can specify different chart types and sizes.
-
-		// Chart property of Shape contains all chart related options.
 		Chart chart = shape.getChart();
 
 		// Get chart series collection.
 		ChartSeriesCollection seriesColl = chart.getSeries();
-
 		// Delete default generated series.
 		seriesColl.clear();
-
 		// Create category names array, in this example we have two categories.
-		String[] categories = new String[] { "대화 빈도 분석" };
-
+		String[] categories = new String[] { "단어 빈도 분석" };
 		// Adding new series. Please note, data arrays must not be empty and arrays must
 		// be the same size.
 		KomoranSearch KS = new KomoranSearch();
-		HashMap<String, List<String>> items = new HashMap<>();
+		ArrayList<WordCount> words = new ArrayList<WordCount>();
+		StringProcessor strProcessor = new StringProcessor(listToStrTotal(chronicleData), filteringList(FILTER), words);
 
+		for (int i = 0; i < strProcessor.words.size(); i++) {
+			seriesColl.add(strProcessor.words.get(i).word, categories, new double[] { strProcessor.words.get(i).n });
+		}
+		// 1번차트(단어 빈도) 끝
+		// 2번 차트(참여자 발화 빈도) 시작
+		HashMap<String, List<String>> items = new HashMap<>();
 		ArrayList<String> particiNames = new ArrayList<>();
 
 		for (int i = 0; i < chronicleData.size(); i++) {
 			List<String> ls = KS.makeKomoran(chronicleData.get(i).getText());
-
-			System.out.println("이것은 ls" + ls);
-
 			// 1. 참석자 이름이 items에 있을 때,
 			if (items.containsKey(chronicleData.get(i).getName())) {
 				for (int j = 0; j < ls.size(); j++) {
@@ -96,16 +146,56 @@ public class Aspose {
 				items.put(chronicleData.get(i).getName(), ls);
 				particiNames.add(chronicleData.get(i).getName());
 			}
-
-			System.out.println("이름 출력" + items.get(chronicleData.get(i).getName()));
 		}
+		Chart chart2 = shape2.getChart();
+		chart2.getSeries().clear();
+
+		String[] particiName = new String[particiNames.size()];
+		double[] particiSize = new double[particiNames.size()];
 
 		for (int i = 0; i < particiNames.size(); i++) {
-			seriesColl.add(particiNames.get(i), categories, new double[] { items.get(particiNames.get(i)).size() });
+			particiName[i] = particiNames.get(i);
+			particiSize[i] = items.get(particiNames.get(i)).size();
 		}
-		
-		//1번차트 끝
 
+		ChartSeries series1 = chart2.getSeries().add("발화자 빈도", particiName, particiSize);
+		ChartDataLabelCollection labels1 = series1.getDataLabels();
+		labels1.setShowPercentage(true);
+		labels1.setShowValue(false);
+		labels1.setShowLeaderLines(false);
+		labels1.setSeparator(" - ");
+		// 2번차트 끝
+
+		// 3번 차트(전체 긍정/부정 빈도 차트) 시작
+		Chart chart3 = shape3.getChart();
+		chart3.getSeries().clear();
+
+		String[] feelName = new String[] { "긍정", "부정" };
+		String tmp = listToStrTotalNotKomo(chronicleData);
+
+		String[] usedWords = tmp.split(" ");
+		double[] feelCnt = new double[2];
+
+		for (int i = 0; i < usedWords.length; i++) {
+			for (int j = 0; j < POSITIVE.length; j++) {
+				if (usedWords[i].contains(POSITIVE[j])) {
+					++feelCnt[0];
+				}
+			}
+			for (int j = 0; j < NEGATIVE.length; j++) {
+				if (usedWords[i].contains(NEGATIVE[j])) {
+					++feelCnt[1];
+				}
+			}
+		}
+		ChartSeries series2 = chart3.getSeries().add("회의 긍정 지수", feelName, feelCnt);
+		ChartDataLabelCollection labels2 = series2.getDataLabels();
+		labels2.setShowPercentage(true);
+		labels2.setShowValue(false);
+		labels2.setShowLeaderLines(false);
+		labels2.setSeparator(" - ");
+		// 3번차트 끝
+		
 		// 여기부터 대화록 테이블
 		Table table = builder.startTable();
 
@@ -143,7 +233,16 @@ public class Aspose {
 		}
 		builder.endRow();
 		builder.endTable();
-
 		doc.save("testData.docx");
 	}
+
+	private static HashSet<String> filteringList(String path) throws IOException {
+		HashSet<String> filter = new HashSet<String>();
+		Scanner scan = new Scanner(new File(path));
+		while (scan.hasNext()) {
+			filter.add(scan.next());
+		}
+		return filter;
+	}
+
 }
--- a301-back/src/main/java/com/chron/wordcloud/words/StringProcessor.java
+++ a301-back/src/main/java/com/chron/wordcloud/words/StringProcessor.java
@@ -8,51 +8,61 @@ import java.util.Map.Entry;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
-public class StringProcessor implements Iterable<WordCount>{
-    private String str;
-    private final HashSet<String> filter;
-    private ArrayList<WordCount> words;
-    
-    public StringProcessor(String str, HashSet<String> filter) {
-        this.str = str;
-        this.filter = filter;
-        processString();
-    }
-    
-    private void processString() {
-        Scanner scan = new Scanner(str);
-        HashMap<String, Integer> count = new HashMap<String, Integer>();
-        while (scan.hasNext()) {
-            String word = removePunctuations(scan.next());
-            if (filter.contains(word)) continue;
-            if (word.equals("")) continue;
-            Integer n = count.get(word);
-            count.put(word, (n == null) ? 1 : n + 1);
-        }
-        PriorityQueue<WordCount> pq = new PriorityQueue<WordCount>();
-        for (Entry<String, Integer> entry : count.entrySet()) {
-            pq.add(new WordCount(entry.getKey(), entry.getValue()));
-        }
-        words = new ArrayList<WordCount>();
-        while (!pq.isEmpty()) {
-            WordCount wc = pq.poll();
-            if (wc.word.length() > 1) words.add(wc);
-        }
-    }
-    
-    public void print() {
-        Iterator<WordCount> iter = iterator();
-        while (iter.hasNext()) {
-            System.out.println(iter.next());
-        }
-    }
+public class StringProcessor implements Iterable<WordCount> {
+	public String str;
+	public HashSet<String> filter;
+	public ArrayList<WordCount> words;
 
-    @Override
-    public Iterator<WordCount> iterator() {
-        return words.iterator();
-    }
-    
-    private static String removePunctuations(String str) {
-        return str.replaceAll("\\p{Punct}|\\p{Digit}", "");
-    }
+	public StringProcessor(String str, HashSet<String> filter, ArrayList<WordCount> words) {
+		this.str = str;
+		this.filter = filter;
+		this.words = processString();
+	}
+	
+	public StringProcessor(String str, HashSet<String> filter) {
+		this.str = str;
+		this.filter = filter;
+		processString();
+	}
+
+	public ArrayList<WordCount> processString() {
+		Scanner scan = new Scanner(str);
+		HashMap<String, Integer> count = new HashMap<String, Integer>();
+		while (scan.hasNext()) {
+			String word = removePunctuations(scan.next());
+			if (filter.contains(word))
+				continue;
+			if (word.equals(""))
+				continue;
+			Integer n = count.get(word);
+			count.put(word, (n == null) ? 1 : n + 1);
+		}
+		PriorityQueue<WordCount> pq = new PriorityQueue<WordCount>();
+		for (Entry<String, Integer> entry : count.entrySet()) {
+			pq.add(new WordCount(entry.getKey(), entry.getValue()));
+		}
+		words = new ArrayList<WordCount>();
+		while (!pq.isEmpty()) {
+			WordCount wc = pq.poll();
+			if (wc.word.length() > 1)
+				words.add(wc);
+		}
+		return words;
+	}
+
+	public void print() {
+		Iterator<WordCount> iter = iterator();
+		while (iter.hasNext()) {
+			System.out.println(iter.next());
+		}
+	}
+
+	@Override
+	public Iterator<WordCount> iterator() {
+		return words.iterator();
+	}
+
+	public static String removePunctuations(String str) {
+		return str.replaceAll("\\p{Punct}|\\p{Digit}", "");
+	}
 }
